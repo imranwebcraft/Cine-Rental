adb8e4c2239ba52335553f25c7c6ae8626848b1d
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-24 12:18:20 +0600

    cart modal added with onclose props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import moon from "../assets/icons/moon.svg";
 import logo from "../assets/logo.svg";
 import ring from "../assets/ring.svg";
 import shoppingCart from "../assets/shopping-cart.svg";
+import CartDetails from "../cine/CartDetails";
 
 const Header = () => {
+	const [showCart, setShowCart] = useState(false);
+
+	const handleShowCart = () => {
+		setShowCart(true);
+	};
+
+	const handleCloseCart = () => {
+		setShowCart(false);
+	};
+
 	return (
 		<header>
+			{showCart && <CartDetails onClose={handleCloseCart} />}
 			<nav className="container flex items-center justify-between space-x-10 py-6">
 				<a href="index.html">
 					<img src={logo} width="139" height="26" alt="" />
@@ -30,10 +43,16 @@ const Header = () => {
 					</li>
 					<li>
 						<a
+							onClick={handleShowCart}
 							className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
 							href="#"
 						>
-							<img src={shoppingCart} width="24" height="24" alt="" />
+							<img
+								src={shoppingCart}
+								width="24"
+								height="24"
+								alt="shopping cart"
+							/>
 						</a>
 					</li>
 				</ul>
