1846d62225aeb1a1fbbcc6b8d971b6d1c2133f10
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-24 17:22:45 +0600

    Make changes to make sure that the UI match with dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 function App() {
 	const [cartData, setCartData] = useState([]);
-	const [theme, setTheme] = useState("light");
+	const [theme, setTheme] = useState("dark");
 
 	const cartInfo = {
 		cartData,
--- src/cine/CartDetails.jsx
+++ src/cine/CartDetails.jsx
@@ -15,7 +15,7 @@ const CartDetails = ({ onClose }) => {
 	return (
 		<div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm">
 			<div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[790px] p-4 max-h-[90vh] overflow-auto">
-				<div className="bg-white shadow-md dark:bg-white rounded-2xl overflow-hidden p-5 md:p-9">
+				<div className="bg-white shadow-md dark:bg-black dark:text-white rounded-2xl overflow-hidden p-5 md:p-9">
 					<h2 className="text-2xl lg:text-[30px] mb-10 font-bold">
 						Your Carts
 					</h2>
@@ -70,7 +70,7 @@ const CartDetails = ({ onClose }) => {
 
 					{/* Action Button */}
 					<div className="flex items-center justify-end gap-2">
-						<button className="rounded-md p-2 md:px-4 inline-flex items-center space-x-2 bg-primary text-[#171923] text-sm">
+						<button className="rounded-md p-2 md:px-4 inline-flex items-center space-x-2 bg-primary text-[#171923] dark:text-white text-sm">
 							<img src={Checkout} width="24" height="24" alt="checkout icon" />
 							<span>Checkout</span>
 						</button>
